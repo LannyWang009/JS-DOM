2ac4461b2e43704184c49965a0f9aa4b15d44156
LannyWang009 <09legend.wang@gmail.com>
2018-10-25 18:39:11 -0500

    ex1-ex4

--- ex4/script.js
+++ ex4/script.js
@@ -1,2 +1,11 @@
 // Make all of the boxes blue when you click the button
 // Notice that all the boxes have "class" instead of "id"
+
+var button = document.getElementById("myButton");
+var allBoxes = document.getElementsByClassName("box");
+
+button.addEventListener('click', function() {
+    allBoxes[0].style.backgroundColor = 'blue';
+    allBoxes[1].style.backgroundColor = 'blue';
+    allBoxes[2].style.backgroundColor = 'blue';
+})
